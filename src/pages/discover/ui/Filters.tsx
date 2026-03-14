25eedef3b061377115a61240f7a372fbf1322d6a
silentesc <scope-frisk-copy@duck.com>
2026-03-14 09:25:06 +0100

    feat(Filters): x button cursor pointer

--- src/pages/discover/ui/Filters.tsx
+++ src/pages/discover/ui/Filters.tsx
@@ -95,7 +95,7 @@ export function Filters({ isOpen, onFiltersChange, onClose }: FiltersProps) {
                     <div className="m-auto">
                     </div>
                     {/* x button */}
-                    <svg onClick={() => onClose()} className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <svg onClick={() => onClose()} className="w-6 h-6 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </div>
