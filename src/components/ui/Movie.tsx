05d1fcf4ff8e452fe7e8dfeca7c19952401d00bf
silentesc <scope-frisk-copy@duck.com>
2026-03-15 21:08:54 +0100

    feat(movie): Change label to blue color

--- src/components/ui/Movie.tsx
+++ src/components/ui/Movie.tsx
@@ -8,7 +8,7 @@ export function Movie({ movie }: MovieProps) {
     return (
         <div className="relative min-w-35 max-w-45 bg-background-primary shadow-[0_0_40px_-10px_rgba(0,0,0,0.5)] border border-background-tertiary rounded-md">
             {/* Label */}
-            <div className="absolute top-1 left-1 bg-purple-600/80 px-2 py-1 rounded text-xs font-semibold z-10">
+            <div className="absolute top-1 left-1 bg-blue-600/80 px-2 py-1 rounded text-xs font-semibold z-10">
                 MOVIE
             </div>
 
