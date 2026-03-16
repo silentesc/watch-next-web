0b554aa3224d2faf93b31450dd231cd3be368ce5
silentesc <scope-frisk-copy@duck.com>
2026-03-16 20:44:19 +0100

    feat: Implement languages endpoint

---
+++ src/api/configuration/languages.ts
@@ -0,0 +1,13 @@
+import { api } from "../client";
+import { error2userMessage } from "../errors";
+import type { Language } from "./models";
+
+
+export async function getLanguages(): Promise<Array<Language>> {
+    try {
+        const response = await api.get<Array<Language>>("/configuration/languages");
+        return response.data;
+    } catch (err) {
+        throw new Error(error2userMessage(err));
+    }
+}
