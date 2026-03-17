c7cdc95d0b8ba8d6dded85e745b044925b2f7566
silentesc <scope-frisk-copy@duck.com>
2026-03-17 16:39:57 +0100

    fix(Input): value prop causing unwritable input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,18 +1,16 @@
 interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "value"> {
     type?: "date" | "datetime-local" | "time" | "email" | "number" | "password" | "search" | "tel" | "text" | "url" | "number";
     placeholder?: string;
-    value?: string | number | readonly string[];
     required?: boolean;
     onChange?: (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => void;
 }
 
-function Input({ type = "text", placeholder = "", value = "", required = false, onChange = () => { }, ...props }: InputProps) {
+function Input({ type = "text", placeholder = "", required = false, onChange = () => { }, ...props }: InputProps) {
     return (
         <input
             type={type}
             required={required}
             placeholder={placeholder}
-            value={value}
             onChange={onChange}
             className="w-full border-background-tertiary border-2 p-2 outline-none bg-background-secondary"
             {...props}
