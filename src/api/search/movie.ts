367d6d13a07e32fd39b7985be664b5f1935a4aa5
silentesc <scope-frisk-copy@duck.com>
2026-04-12 10:44:13 +0200

    ref: Put search movie query into hook

--- src/api/search/movie.ts
+++ src/api/search/movie.ts
@@ -9,7 +9,7 @@ export interface SearchMovieResponse {
     results: Array<MovieOverview>;
 }
 
-export async function search_movie(query: string, page: number = 1): Promise<SearchMovieResponse> {
+export async function search_movie(query: string, page?: number): Promise<SearchMovieResponse> {
     try {
         const response = await api.get<SearchMovieResponse>("/search/movie", { params: { query, page } });
         return response.data;
