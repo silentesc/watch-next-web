892670cf50680887905a78cfff94be0851b36188
silentesc <scope-frisk-copy@duck.com>
2026-09-15 18:26:11 +0200

    refactor: fix copy paste dead code from some movie api impls

--- src/api/movie/recommendations.ts
+++ src/api/movie/recommendations.ts
@@ -1,11 +1,6 @@
 import { api } from "../client";
 import { error2userMessage } from "../errors";
-import type { MovieOverview, ReleaseDate } from "../models";
-
-export interface ReleaseDateResult {
-    iso_3166_1: string;
-    release_dates: Array<ReleaseDate>;
-}
+import type { MovieOverview } from "../models";
 
 export interface MovieRecommendationsResponse {
     page: number;
