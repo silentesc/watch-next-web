e8c3fbc4f083e42e6b7c0e1b402985a64c1a7b55
silentesc <scope-frisk-copy@duck.com>
2026-09-18 23:16:31 +0200

    fix: add placeholder image when tv episode still is not available

--- src/components/ui/TvEpisode.tsx
+++ src/components/ui/TvEpisode.tsx
@@ -9,13 +9,22 @@ interface TvEpisodeProps {
 export function TvEpisode({ episode }: TvEpisodeProps) {
     return (
         <li className="flex flex-col gap-3 py-4 first:pt-3 last:pb-3 sm:flex-row">
-            {episode.still_path && (
-                <img
-                    className="aspect-video w-35 rounded object-contain sm:w-28"
-                    src={`https://image.tmdb.org/t/p/w300${episode.still_path}`}
-                    alt={episode.name}
-                />
-            )}
+            <div className="aspect-video w-35">
+                {episode.still_path ? (
+                    <div className="flex w-full h-full items-center justify-center">
+                        <img
+                            className="w-full h-full object-contain sm:w-28"
+                            src={`https://image.tmdb.org/t/p/w300${episode.still_path}`}
+                            alt={episode.name}
+                        />
+                    </div>
+                ) : (
+                    <div className="flex w-full h-full items-center justify-center">
+                        <img className="w-full h-full rounded-l-md object-contain  grayscale opacity-30" src="/sad_logo.png" alt={episode.name} />
+                    </div>
+                )
+                }
+            </div>
             <div className="min-w-0 flex-1">
                 <div className="flex flex-wrap items-baseline gap-x-2 gap-y-1">
                     <span className="text-lg">
